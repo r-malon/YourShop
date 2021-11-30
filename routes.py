8a36434720a60e73c991d6add43f7eaef5783510
Rafael Malon <33752131+r-malon@users.noreply.github.com>
2021-11-29 22:50:57 -0300

    Added Watson webchat

--- routes.py
+++ routes.py
@@ -127,4 +127,4 @@ def not_found(e):
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000, debug=True, ssl_context='adhoc')
+	app.run(host='0.0.0.0', port=5000, debug=True) #, ssl_context='adhoc')
